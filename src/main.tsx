f2894ef34110517dc227ff73f5d8176198006b54
zxc <kosarev99@gmail.com>
2023-04-30 22:33:51 +0400

    imports fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "./assets/styles/index.css";
 import { store } from "./store";
 import { Provider } from "react-redux";
 
